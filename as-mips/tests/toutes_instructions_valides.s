77092e998eeab50bf4fd5d7dd9cd33795333a943
Sébastien TAURAND <sebastien.taurand@phelma.grenoble-inp.fr>
2017-10-22 10:23:27 +0200

    Mise en place du test de toutes les instructions

---
+++ as-mips/tests/toutes_instructions_valides.s
@@ -0,0 +1,49 @@
+.text
+ADD $0,$4,$13
+ADDI $1, $3, 21
+ADDI $1, $3, nombre
+SUB $2,$5,$6
+MULT $7,$8
+DIV $9,$10
+AND $11,$12,$13
+OR $14,$15,$16
+XOR $17,$18,$19
+ROTR $20, $21, 0
+ROTR $20, $21, 31
+ROTR $20, $21, toto
+SLL $22, $23, 12
+SLL $24, $25, tata
+SRL $26, $27, tata
+SRL $26, $27, 30
+SLT $28,$29,$30
+LW $31, 0x60($10)
+SW $zero, 45($a0)
+LUI $t1, 12
+MFHI $s2
+MFLO $k1
+BEQ $1, $3, 2398
+BEQ $gp, $sp, nombre
+BNE $ra, $t9, nombre
+BGTZ $23, 14
+BLEZ $t5, 50
+J 0x3847
+J nombre
+JAL 0x3847
+JR $zero
+NOP
+MOVE $1, $2
+#NEG $3, $4
+#LI $sp, 100
+#LI $sp, nmbre
+#BLT $10, $11, 0x1356
+.data
+.byte 45
+.byte 0x1A,023
+.word 32000
+.word -1000,-0400,-0x1
+.space 15
+.asciiz "test"
+.asciiz "", "\"""
+.bss
+.space 12
+.space 05,0xF
